86d594805b3426c13d2134ac4d94fe586f595862
cerekson <cody@erekson.org>
2013-05-24 14:42:37 -0600

    Changed voice script into module

--- voice.js
+++ voice.js
@@ -1,8 +1,22 @@
 var fs = require('fs');
 var lame = require('lame');
-var Speaker = require('speaker');
- 
-fs.createReadStream(process.argv[2])
-  .pipe(new lame.Decoder)
-  .on('format', console.log)
-  .pipe(new Speaker); 
+var speaker = require('speaker');
+
+//function to play given audio file
+function playVoice(voiceFile){
+	return {
+
+		//play function
+		play: function play() {
+			console.log("Playing: " + voiceFile);
+			var vn = "voice/" + voiceFile;
+			fs.createReadStream(fn)
+			  .pipe(new lame.Decoder)
+			  .on('format', console.log)
+			  .pipe(new speaker);
+		},
+	};
+}
+
+//Export as a module
+module.exports = playVoice;
